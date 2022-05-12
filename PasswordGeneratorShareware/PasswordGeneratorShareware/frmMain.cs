8a33f15b213185aedc9c4fa2df5c98a569148550
Lika <shelley.bass.li@gmail.com>
2022-05-12 11:01:49 +0300

    Refresh projects
    Удалена старая версия работы с реестром, добавлена программа с триальнной версией и описаны результаты выполнения в readme.

---
+++ PasswordGeneratorShareware/PasswordGeneratorShareware/frmMain.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordGeneratorShareware
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        // Работа самого приложения
+
+        int[] IntCount = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        string[] SpecialItems = { "%", "*", "(", ")", "?", "!", "~", "#", "^", "&" };
+        Random dice = new Random();
+
+        private void btnPasswordGenerated_Click(object sender, EventArgs e)
+        {
+            if (cldPassword.CheckedItems.Count == 0) return;
+            string PSW = "";
+            for (int i = 0; i < nudPasswordLength.Value; i++)
+            {
+                int n = dice.Next(0, cldPassword.CheckedItems.Count);
+                string s = cldPassword.CheckedItems[n].ToString();
+                switch (s)
+                {
+                    case "Цифры":
+                        PSW += IntCount[dice.Next(IntCount.Length)].ToString();
+                        break;
+
+                    case "Прописные буквы":
+                        PSW += Convert.ToChar(dice.Next(65, 88));
+                        break;
+
+                    case "Строчные буквы":
+                        PSW += Convert.ToChar(dice.Next(97, 122));
+                        break;
+
+                    case "Спец. символы: %, *, ), ?, !, ~, #, ^, &":
+                        PSW += SpecialItems[dice.Next(SpecialItems.Length)];
+                        break;
+                }
+            }
+            txtbPasswordGenerated.Text = PSW;
+        }
+
+        //Shareware 
+        private void firstSettings()
+        {
+            if (Registry.CurrentUser.OpenSubKey(@"Software\Шамугия") == null)
+            {
+                Registry.CurrentUser.CreateSubKey(@"Software\Шамугия");
+            }
+            Properties.Settings.Default.IsFirstRun = false;
+            Properties.Settings.Default.RunCount = 0;
+            Properties.Settings.Default.Save();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.IsFirstRun)
+            {
+                firstSettings();
+
+                RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Шамугия", true);
+                key.SetValue("RunCount", Properties.Settings.Default.RunCount + 1, RegistryValueKind.DWord);
+                key.Close();
+            }
+        }
+
+        private void frmMain_Shown(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.RunCount != 0)
+            {
+
+                frmShareware sharewareForm = new frmShareware();
+                sharewareForm.ShowDialog();
+
+                RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Шамугия", true);
+                key.SetValue("RunCount", Properties.Settings.Default.RunCount);
+                key.Close();
+
+            }
+            else
+            {
+                MessageBox.Show("Вы пользуетесь пробной версией программы. Ограничение пробной версии: " + Properties.Settings.Default.DemoVersionCount + " запусков программы. " +
+                    "Для полнофункционального пользования приобретите лицензию.");
+                Properties.Settings.Default.RunCount++;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+    }
+}
---
+++ PasswordGeneratorShareware/PasswordGeneratorShareware/frmShareware.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace PasswordGeneratorShareware
+{
+    partial class frmShareware
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTrialInformation = new System.Windows.Forms.Label();
+            this.lblRunCount = new System.Windows.Forms.Label();
+            this.btnRunCountFamiliarized = new System.Windows.Forms.Button();
+            this.lblBeforeTrialEnd = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTrialInformation
+            // 
+            this.lblTrialInformation.Location = new System.Drawing.Point(21, 57);
+            this.lblTrialInformation.Name = "lblTrialInformation";
+            this.lblTrialInformation.Size = new System.Drawing.Size(341, 48);
+            this.lblTrialInformation.TabIndex = 7;
+            this.lblTrialInformation.Text = "Для того чтобы пользоваться функионалом программы по истичении пробного периода, " +
+    "вам необходимо приобрести лицензию.";
+            this.lblTrialInformation.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblRunCount
+            // 
+            this.lblRunCount.Location = new System.Drawing.Point(240, 14);
+            this.lblRunCount.Name = "lblRunCount";
+            this.lblRunCount.Padding = new System.Windows.Forms.Padding(0, 0, 0, 5);
+            this.lblRunCount.Size = new System.Drawing.Size(107, 45);
+            this.lblRunCount.TabIndex = 6;
+            this.lblRunCount.Text = "N запусков";
+            this.lblRunCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnRunCountFamiliarized
+            // 
+            this.btnRunCountFamiliarized.Location = new System.Drawing.Point(142, 113);
+            this.btnRunCountFamiliarized.Name = "btnRunCountFamiliarized";
+            this.btnRunCountFamiliarized.Size = new System.Drawing.Size(92, 34);
+            this.btnRunCountFamiliarized.TabIndex = 5;
+            this.btnRunCountFamiliarized.Text = "Ознакомлен";
+            this.btnRunCountFamiliarized.UseVisualStyleBackColor = true;
+            this.btnRunCountFamiliarized.Click += new System.EventHandler(this.btnRunCountFamiliarized_Click);
+            // 
+            // lblBeforeTrialEnd
+            // 
+            this.lblBeforeTrialEnd.AutoSize = true;
+            this.lblBeforeTrialEnd.Location = new System.Drawing.Point(36, 30);
+            this.lblBeforeTrialEnd.Name = "lblBeforeTrialEnd";
+            this.lblBeforeTrialEnd.Size = new System.Drawing.Size(198, 13);
+            this.lblBeforeTrialEnd.TabIndex = 4;
+            this.lblBeforeTrialEnd.Text = "Запусков до конца пробного периода";
+            // 
+            // frmShareware
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 161);
+            this.Controls.Add(this.lblTrialInformation);
+            this.Controls.Add(this.lblRunCount);
+            this.Controls.Add(this.btnRunCountFamiliarized);
+            this.Controls.Add(this.lblBeforeTrialEnd);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "frmShareware";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Условно-бесплатное использование программы";
+            this.Load += new System.EventHandler(this.frmShareware_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTrialInformation;
+        private System.Windows.Forms.Label lblRunCount;
+        private System.Windows.Forms.Button btnRunCountFamiliarized;
+        private System.Windows.Forms.Label lblBeforeTrialEnd;
+    }
+}
